0f4d5efddee138e219a36031a0e97f567f5c3e82
ts0yu <ts0yu@proton.me>
2023-02-03 17:40:45 +0000

    latest

--- crates/simulate/src/execution.rs
+++ crates/simulate/src/execution.rs
@@ -5,6 +5,9 @@ use revm::{
 };
 use bytes::Bytes;
 
+/// Uniswap V3 factory address.
+const FACTORY: &str = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
+
 #[derive(Debug, Default)]
 pub struct ExecutionManager {
     pub evm: EVM<CacheDB<EmptyDB>>,
@@ -16,20 +19,17 @@ impl ExecutionManager {
         let mut evm = EVM::new();
         let mut db = CacheDB::new(EmptyDB {});
 
-        db.insert_account_info(uniswap, AccountInfo {
-            balance: U256::zero(),
-            code: Bytes..
-            nonce: 0
-            storage
-        })
         evm.database(db);
 
         Self { evm }
     }
 
+    /// Deploy a contract to the Execution instance.
+    fn deploy_contract(&mut self, bytecode: Bytecode) {
+        let factory_info = AccountInfo::new(U256::zero(), 0, bytecode);
 
-
-    
+        self.evm.db.insert_account_info(B160::from_str(FACTORY), factory_info);
+    }
 
     // pub fn execute(
     //     transact_to: 
